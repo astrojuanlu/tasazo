544d207968cf692e9a86e1e131c51cf032c600a0
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2012-05-15 12:06:05 +0200

    Añadida comparativa de precios, información sobre los precios del crédito

--- js/script.js
+++ js/script.js
@@ -2,26 +2,38 @@
 
 */
 
+// FIXME: Este código es penoso, y lo sé. Estoy repitiendo un montón de líneas.
+// Ya habrá tiempo de solucionarlo, si la cosa funciona.
+
 // Precios de los créditos
 // Obtenido de la información que dio Diego en la lista de correo
-var p1 = 30.0;
-var p2 = 48.0;
-var p3 = 108.0;
-var p4 = 120.0;
+var p1a = 17.01;
+var p2a = 21.26;
+var p3a = 28.92;
+var p4a = 28.92;
+
+var p1n = 30.0;
+var p2n = 48.0;
+var p3n = 90.0;
+var p4n = 120.0;
 
 $(document).ready(function(){
 	// Calcula el precio de la matrícula nada más cargar la página
-	p = calcular()
-	$("#resultado").text(p.toFixed(2) + ' €');
+	pn = calcular_nuevo()
+	pa = calcular_antiguo()
+	$("#resultado .new").text(pn.toFixed(2) + ' €');
+	$("#resultado .old").text(pa.toFixed(2) + ' €');
 	
 	// Recalcula el precio al cambiar los valores del formulario
 	$("#calculo input").change(function(event){
-		p = calcular()
-		$("#resultado").text(p.toFixed(2) + ' €');
+		pn = calcular_nuevo()
+		pa = calcular_antiguo()
+		$("#resultado .new").text(pn.toFixed(2) + ' €');
+		$("#resultado .old").text(pa.toFixed(2) + ' €');
 	});
 });
 
-function calcular()
+function calcular_nuevo()
 {
 	// Recalcula el nuevo precio final de la matrícula
 	c1 = Number($("#matricula1").val());
@@ -29,7 +41,18 @@ function calcular()
 	c3 = Number($("#matricula3").val());
 	c4 = Number($("#matricula4").val());
 	
-	return c1 * p1 + c2 * p2 + c3 * p3 + c4 * p4;
+	return c1 * p1n + c2 * p2n + c3 * p3n + c4 * p4n;
+}
+
+function calcular_antiguo()
+{
+	// Recalcula el nuevo precio final de la matrícula
+	c1 = Number($("#matricula1").val());
+	c2 = Number($("#matricula2").val());
+	c3 = Number($("#matricula3").val());
+	c4 = Number($("#matricula4").val());
+	
+	return c1 * p1a + c2 * p2a + c3 * p3a + c4 * p4a;
 }
 
 
